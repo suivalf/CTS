ae426867d39efd6444d58d797476c23296329df8
suivalf <cirstian_flaviusmihai@yahoo.com>
2021-05-25 00:13:30 +0300

    aaa

--- app/__init__.py
+++ app/__init__.py
@@ -32,3 +32,6 @@ if not app.debug:
             credentials=auth, secure=secure)
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
+
+if __name__ == '__main__':
+    app.run(debug=True)
